7f170d42d9e36cc7ee39e12775bac4995b2ef1c8
yogesh kumar <yogeshkhatri697@gmail.com>
2025-03-11 17:04:01 +0530

    Solution to Toll Plaza Management System

---
+++ Toll Plaza Management System@@67877d61dc107486ad5c2750/code.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+typedef struct vehicle{
+    char number[50];
+    char type[50];
+    int  amount;
+
+}v;
+int main(){
+    int n;
+    scanf("%d",&n);
+    v vehicles[100];
+    for(int i=0;i<n;i++){
+        scanf("%s %s %d",&vehicles[i].number,&vehicles[i].type,&vehicles[i].amount);
+    }
+    float car_toll=0;
+    float truck_toll=0;
+    float bike_toll=0;
+    for(int i=0;i<n;i++){
+        if(vehicles[i].type=='Car'){
+            car_toll+=vehicles[i].toll;
+        }
+        elif(vehicles[i].type=='Truck'){
+            truck_toll+=vehicles[i].toll;
+        }
+        elif(vehicles[i].type=='Bike'){
+            bike_toll+=vehicles[i].toll;
+        }
+    }
+    printf("Cars: %.2f, Trucks: %.2f, Bikes: %.2f",car_toll,truck_toll,bike_toll);
+
+}

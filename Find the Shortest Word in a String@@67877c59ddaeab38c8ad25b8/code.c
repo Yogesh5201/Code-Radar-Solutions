7058e1eea579780b373bd6e3ee4331b8822859e0
yogesh kumar <yogeshkhatri697@gmail.com>
2025-03-12 14:56:39 +0530

    Solution to Find the Shortest Word in a String

--- Find the Shortest Word in a String@@67877c59ddaeab38c8ad25b8/code.c
+++ Find the Shortest Word in a String@@67877c59ddaeab38c8ad25b8/code.c
@@ -1,29 +1,29 @@
+#include <stdio.h>
+#include <string.h>
 
-#include<stdio.h>
-#include<string.h>
-int main(){
+int main() {
     char str[100];
-    scanf("%[^\n]",str);
-    char word1[30] ,word2[30],word3[30],word4[30];
-    int count = sscanf(str, "%s %s %s %s", word1, word2, word3,word4);
-    if (count < 4) word4[0] = '\0';
-    if (count < 3) word3[0] = '\0';
-    if (count < 2) word2[0] = '\0';
-    if (count < 1) word1[0] = '\0';
-    int n1=strlen(word1);
-    int n2=strlen(word2);
-    int n3=strlen(word3);
-    int n4=strlen(word4);
-    if(n1<n2 && n1<n3 && n1<n4){
-        printf("%s",word1);
+    scanf("%[^\n]", str);
+
+    char word1[30] = "", word2[30] = "", word3[30] = "", word4[30] = "";
+    int count = sscanf(str, "%s %s %s %s", word1, word2, word3, word4);
+
+    int minLen = strlen(word1);
+    char *minWord = word1;
+
+    if (count > 1 && strlen(word2) < minLen) {
+        minLen = strlen(word2);
+        minWord = word2;
     }
-    else if(n2<n1 && n2<n3 &&n2<n4){
-        printf("%s",word2);
+    if (count > 2 && strlen(word3) < minLen) {
+        minLen = strlen(word3);
+        minWord = word3;
     }
-    else if(n3<n1 && n3<n2 && n3<n4){
-        printf("%s",word3);
+    if (count > 3 && strlen(word4) < minLen) {
+        minLen = strlen(word4);
+        minWord = word4;
     }
-    else if(n4<n1 && n4<n2 && n4<n3){
-        printf("%s",word4);
-    }
+
+    printf("%s", minWord);
+    return 0;
 }

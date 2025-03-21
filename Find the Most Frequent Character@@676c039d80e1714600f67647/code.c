05c5347bf5328cce9d5cb345386e3e03016ba5d3
yogesh kumar <yogeshkhatri697@gmail.com>
2025-03-21 11:34:12 +0530

    Solution to Find the Most Frequent Character

--- Find the Most Frequent Character@@676c039d80e1714600f67647/code.c
+++ Find the Most Frequent Character@@676c039d80e1714600f67647/code.c
@@ -1,40 +1,26 @@
-#include <stdio.h>
-#include <string.h>
-
-int main() {
+#include<stdio.h>
+#include<string.h>
+int main(){
     char str[100];
-    scanf("%[^\n]", str);
-
-    int n = strlen(str);
-  
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j < n - i - 1; j++) {
-            if (str[j] > str[j + 1]) {
-                char temp = str[j];
-                str[j] = str[j + 1];
-                str[j + 1] = temp;
+    scanf("%[^\n]",str);
+    int freq[100];
+    int n=strlen(str);
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(str[i]==str[j]){
+                count++;
             }
+        }freq[i]=count;
+    }
+    int max=freq[0];
+    for(int i=0;i<n;i++){
+        if(freq[i]>=max){
+            max=freq[i];
+            int a=i;
         }
     }
+    printf("%d",str[a]);
 
-    int max = 0;
-    char mostfrequent;
-    for (int i = 0; i < n; i++) {
-        if (str[i] == ' ') continue;
-        
-        int count = 1;
-        while (i + 1 < n && str[i] == str[i + 1]) {
-            count++;
-            i++;
-        }
-
-        if (count > max) {
-            max = count;
-            mostfrequent = str[i];
-        }
-    }
-
-    printf("%c", mostfrequent);
-
-    return 0;
+    
+    
 }

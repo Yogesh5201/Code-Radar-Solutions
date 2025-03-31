70ef997816b136e6bdbf2a715e220b02b433da65
yogesh kumar <yogeshkhatri697@gmail.com>
2025-03-31 18:24:47 +0530

    Solution to Find the First Peak Element

--- Find the First Peak Element@@676c02002dbc8c3e7a280b6e/code.c
+++ Find the First Peak Element@@676c02002dbc8c3e7a280b6e/code.c
@@ -1,36 +1,26 @@
-#include <stdio.h>
-
-int main() {
+#include<stdio.h>
+int main(){
     int n;
-    scanf("%d", &n);
+    scanf("%d",&n);
     int arr[n];
-
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &arr[i]);
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
     }
-
-    if (n == 1) {
-        printf("%d", arr[0]);
-        return 0;
+    if(n<2){
+        printf("%d",arr[0]);
+        break;
     }
-    if (arr[0] > arr[1]) {
-        printf("%d", arr[0]);
-        return 0;
+    if(arr[0]>arr[1]){
+        printf("%d",arr[0]);
+        break;
     }
-    for (int i = 1; i < n - 1; i++) {
-        if (arr[i] > arr[i - 1] && arr[i] > arr[i + 1]) {
-            printf("%d", arr[i]);
-            return 0;
+    for(int i=1;i<n-1;i++){
+        if(arr[i]>arr[i+1]&&arr[i]>arr[i-1]){
+            printf("%d",arr[i]);
+            break;
         }
+    }if(arr[n-1]>arr[arr[n-2]]){
+        printf("%d",arr[n-1]);
+        break;
     }
-    if (arr[n - 1] > arr[n - 2]) {
-        printf("%d", arr[n - 1]);
-        return 0;
-    }else{
-        printf("-1");
-    }
-
-
-    return 0;
 }
-
